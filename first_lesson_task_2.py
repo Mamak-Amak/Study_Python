dfa7b480205e6b8738782e57b310324fb9119bef
Ivan Ystyuzhanin <sam.and.m@yandex.ru>
2022-05-22 20:38:01 +0500

    commit_1

--- first_lesson_task_2.py
+++ first_lesson_task_2.py
@@ -1,10 +1,8 @@
-i=[]
-i = list(range(1, 1000, 2))
-print(i)
+
 for i in range(1, 1000, 2):
+    print(i**3)
 
-    if (i + 17)**3 / 7:
-        print(list(range))
-    #if not (i + 17)**3 / 7:
+for d in range(1, 1000, 2):
 
-
+    d = (i + 17)**3 % 7
+    print(d)
