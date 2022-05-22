eebb274edbbc418abec46a81268aa500b0c7648e
Ivan Ystyuzhanin <sam.and.m@yandex.ru>
2022-05-22 15:44:42 +0500

    commit_1

--- first_lesson_task_2.py
+++ first_lesson_task_2.py
@@ -2,4 +2,9 @@ i=[]
 i = list(range(1, 1000, 2))
 print(i)
 for i in range(1, 1000, 2):
-    if (i + 17)/7, true
+
+    if (i + 17)**3 / 7:
+        print(list(range))
+    #if not (i + 17)**3 / 7:
+
+
